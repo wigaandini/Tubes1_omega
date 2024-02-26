3d042e31cde52428774a0a9daedc0b61ef8b0d5c
b33rk <brkmanics@gmail.com>
2024-02-26 20:55:21 +0700

    update mainBot 2

--- mainBot.py
+++ mainBot.py
@@ -7,18 +7,30 @@ from game.models import GameObject, Board, Position
 from ..util import get_direction
 
 def nearestDiamond(board_bot: GameObject, board: Board) :
-    diamond_positions = [diamond for diamond in board.game_objects if diamond.type == 'DiamondGameObject']
+    diamonds = [diamond for diamond in board.game_objects if diamond.type == 'DiamondGameObject']
     positionBot = board_bot.position
-    blue_diamond_distance = [(abs(B.position.x - positionBot.x) + abs(B.position.y - positionBot.y), B.position) for B in diamond_positions if B.properties.points == 1]
-    red_diamond_distance = [(abs(B.position.x - positionBot.x) + abs(B.position.y - positionBot.y), B.position) for B in diamond_positions if B.properties.points == 2]
+    blue_diamond_distance = [(abs(B.position.x - positionBot.x) + abs(B.position.y - positionBot.y), B.position) for B in diamonds if B.properties.points == 1]
+    red_diamond_distance = [(abs(B.position.x - positionBot.x) + abs(B.position.y - positionBot.y), B.position) for B in diamonds if B.properties.points == 2]
 
     sorted_blue_diamond_distance = sorted(blue_diamond_distance, key=lambda x: x[0])
     sorted_red_diamond_distance = sorted(red_diamond_distance, key=lambda x: x[0])
 
-    if sorted_red_diamond_distance[0] < sorted_blue_diamond_distance[1]:
-        next_position = sorted_red_diamond_distance[0][1]
-    else:
+    if len(red_diamond_distance) == 0 :
         next_position = sorted_blue_diamond_distance[0][1]
+    elif len(blue_diamond_distance) == 0 :
+        next_position = sorted_red_diamond_distance[0][1]
+    else :
+        print("red ",sorted_red_diamond_distance[0][0])
+        print("blue ",sorted_blue_diamond_distance[0][0])
+        print("size ",board_bot.properties.diamonds)
+        if len(blue_diamond_distance) == 1:
+            x = 0
+        else :
+            x = 1
+        if sorted_red_diamond_distance[0][0] < sorted_blue_diamond_distance[x][0] and board_bot.properties.diamonds <= 3:
+            next_position = sorted_red_diamond_distance[0][1]
+        else:
+            next_position = sorted_blue_diamond_distance[0][1]
     return next_position
 
 class PrototypeLogic(BaseLogic):
