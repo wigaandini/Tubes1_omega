869ef6edfa53ea55e89eb659fd9c2b819094c4aa
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-03-09 12:34:24 +0700

    rapihin code

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/omegaBot.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/omegaBot.py
@@ -4,7 +4,7 @@ from game.logic.base import BaseLogic
 from game.models import GameObject, Board, Position
 from ..util import get_direction
 
-class OmegaLogic(BaseLogic):
+class UltimateLogic2(BaseLogic):
     def __init__(self): # Inisialisasi
         self.goal_position: Optional[Position] = None
         self.portal: tuple[Position, Position]
@@ -19,11 +19,11 @@ class OmegaLogic(BaseLogic):
         return (abs(B.x - A.x) + abs(B.y - A.y))
     
 
-    def isInDomain(self, X : Position, Y: Position, A: Position): # Mengecek apakah objek berada di dalam domain X dan Y
+    def isInDomain(self, X : Position, Y: Position, A: Position):  # Mengecek apakah A berada di dalam domain X dan Y
         return (X.x >= A.x >= Y.x or X.x <= A.x <= Y.x) and (X.y >= A.y >= Y.y or X.y <= A.y <= Y.y) and (X.x != A.x and X.y != A.y) and (Y.x != A.x and Y.y != A.y)
 
 
-    def getPortal(self, board_bot: GameObject, board : Board) : # Mencari posisi portal awal yang terdekat dengan bot, return berupa posisi portal awal dan portal akhir
+    def getPortal(self, board_bot: GameObject, board : Board) : # Mendapatkan posisi portal awal yang terdekat dari bot dan posisi portal kedua yang terjauh dari bot
         portal_position = [d.position for d in board.game_objects if d.type == 'TeleportGameObject']
         bot_position = board_bot.position
         if (self.getDistance(portal_position[0], bot_position) < self.getDistance(portal_position[1], bot_position)):
@@ -35,7 +35,7 @@ class OmegaLogic(BaseLogic):
         return nearest_portal, second_portal
     
 
-    def logicPortal(self, diamonds_position : list[Position], board_bot: GameObject) : # Mencari jarak antara bot dengan diamond melalui portal
+    def logicPortal(self, diamonds_position : list[Position], board_bot: GameObject) : # Memilih jarak dari bot ke diamond melalui portal/tidak 
         distance_from_bot = self.getDistance(diamonds_position[-1], board_bot.position)
         distance_with_portal = self.getDistance(self.portal[0], board_bot.position) + self.getDistance(self.portal[1], diamonds_position[-1])
         if distance_with_portal < distance_from_bot :
@@ -46,26 +46,27 @@ class OmegaLogic(BaseLogic):
             temp_final_distance = distance_from_bot
         return temp_final_distance, result
     
-    def logicDiamond(self, diamonds_position : list[Position], board_bot: GameObject) : # Mencari jarak antara bot dengan diamond
+
+    def logicDiamond(self, diamonds_position : list[Position], board_bot: GameObject) : # Mencari jarak bot ke diamond
         distance_from_bot = self.getDistance(diamonds_position[-1], board_bot.position)
         return distance_from_bot
 
 
-    # Mencari diamond terdekat dengan bot menggunakan algoritma greedy
+    # Mencari jarak terdekat dari bot ke diamond
     def greedyNearestDiamond(self, blue_diamonds_position : list[Position], red_diamonds_position : list[Position], board_bot: GameObject, board: Board) : 
         goal_blue : Optional[Position] = None
         goal_red : Optional[Position] = None
         leng_blue = len(blue_diamonds_position)
         leng_red = len(red_diamonds_position)
-
+        
         if (leng_blue != 0) :
             final_distance_blue = self.getDistance(blue_diamonds_position[-1], board_bot.position)
             goal_blue = blue_diamonds_position[-1]
-
+        
         if (leng_red != 0) :
             final_distance_red = self.getDistance(red_diamonds_position[-1], board_bot.position)
             goal_red = red_diamonds_position[-1]
-
+        
         while leng_blue != 0 or leng_red != 0:
             if (leng_blue != 0) :
                 temp_blue = self.logicDiamond(blue_diamonds_position, board_bot)
@@ -74,7 +75,7 @@ class OmegaLogic(BaseLogic):
                     goal_blue = blue_diamonds_position[-1]
                 blue_diamonds_position.pop()
                 leng_blue -= 1
-
+            
             if (leng_red != 0) :
                 temp_red = self.logicDiamond(red_diamonds_position, board_bot)
                 if temp_red < final_distance_red :
@@ -82,43 +83,48 @@ class OmegaLogic(BaseLogic):
                     goal_red = red_diamonds_position[-1]
                 red_diamonds_position.pop()
                 leng_red -= 1
-
+        
         if (goal_blue != None and goal_red != None) :
-            if self.distance_to_base < final_distance_blue and self.distance_to_base < final_distance_blue and board_bot.position != board_bot.properties.base and board_bot.properties.diamonds >= 3:
-                if (board_bot.position in self.portal) :
-                    self.goal_position = board_bot.properties.base
-                else :
-                    self.logicBasePortal(board_bot, board)
-            
-            elif self.distance_to_redButton < final_distance_blue and self.distance_to_redButton < final_distance_blue:
-                if (board_bot.position in self.portal) :
-                    self.goal_position = self.redButton_position[0]
-                else :
-                    self.logicRedButtonPortal(board_bot, board)
-            
+            if final_distance_red <= final_distance_blue and board_bot.properties.diamonds != 4:
+                final_distance_diamond = final_distance_red
+                self.goal_position = goal_red
             else:
-                if final_distance_red <= final_distance_blue and board_bot.properties.diamonds != 4:
-                    self.goal_position = goal_red
-                else:
-                    self.goal_position = goal_blue
+                final_distance_diamond = final_distance_blue
+                self.goal_position = goal_blue
         
         elif goal_blue == None :
+            final_distance_diamond = final_distance_red
             self.goal_position = goal_red
         
         else :
+            final_distance_diamond = final_distance_blue
             self.goal_position = goal_blue
         
-    
-    def logicBasePortal (self, board_bot : GameObject, board : Board) : # Mencari jarak terdekat antara bot dengan base melalui portal
+        if self.distance_to_base <= final_distance_diamond and board_bot.position != board_bot.properties.base and board_bot.properties.diamonds >= 3:
+            if (board_bot.position in self.portal) :
+                self.goal_position = board_bot.properties.base
+            else :
+                self.logicBasePortal(board_bot, board)
+        
+        elif self.distance_to_redButton <= final_distance_diamond:
+            if (board_bot.position in self.portal) :
+                self.goal_position = self.redButton_position[0]
+            else :
+                self.logicRedButtonPortal(board_bot, board)
+        
+
+    # Memilih jarak terdekat dari bot ke base melalui portal/tidak    
+    def logicBasePortal (self, board_bot : GameObject, board : Board) :
         bot_position = board_bot.position
         base = board_bot.properties.base
         if (self.getDistance(bot_position, self.portal[0]) + self.getDistance(self.portal[1], base) <= self.getDistance(bot_position, base)):
             self.goal_position = self.portal[0]
         else :
             self.goal_position = base
+        
 
-
-    def logicRedButtonPortal (self, board_bot : GameObject, board : Board) : # Mencari jarak terdekat antara bot dengan red button melalui portal
+    # Memilih jarak terdekat dari bot ke redButton melalui portal/tidak    
+    def logicRedButtonPortal (self, board_bot : GameObject, board : Board) :
         bot_position = board_bot.position
         redButton = self.redButton_position[0]
         if (self.getDistance(bot_position, self.portal[0]) + self.getDistance(self.portal[1], redButton) <= self.getDistance(bot_position, redButton)):
@@ -127,8 +133,8 @@ class OmegaLogic(BaseLogic):
             self.goal_position = redButton
 
 
-    # Mencari diamond terdekat dengan bot melalui portal dengan algoritma greedy
-    def greedyDiamondPortal(self, blue_diamonds_position : list[Position], red_diamonds_position : list[Position], board_bot: GameObject, board: Board) : 
+    # Mencari jarak terdekat dari bot ke diamond melalui portal/tidak dengan menggunakan algoritma greedy
+    def greedyDiamondPortal(self, blue_diamonds_position : list[Position], red_diamonds_position : list[Position], board_bot: GameObject, board: Board) :
         goal_position_blue : Optional[Position] = None
         goal_position_red : Optional[Position] = None
         leng_blue = len(blue_diamonds_position)
@@ -141,7 +147,7 @@ class OmegaLogic(BaseLogic):
         if (leng_red != 0) :
             goal_position_red = red_diamonds_position[-1]
             final_distance_red = self.getDistance(red_diamonds_position[-1], board_bot.position)
-        
+       
         while leng_blue != 0 or leng_red != 0:
             if (leng_blue != 0) :
                 temp_position_blue = self.logicPortal(blue_diamonds_position, board_bot)
@@ -168,51 +174,51 @@ class OmegaLogic(BaseLogic):
                 leng_red -= 1
 
         if (goal_position_blue != None and goal_position_red != None) :
-            if self.distance_to_base < final_distance_blue and self.distance_to_base < final_distance_blue and board_bot.position != board_bot.properties.base and board_bot.properties.diamonds >= 3:
-                if (board_bot.position in self.portal) :
-                    self.goal_position = board_bot.properties.base
-                else :
-                    self.logicBasePortal(board_bot, board)
-            
-            elif self.distance_to_redButton < final_distance_blue and self.distance_to_redButton < final_distance_blue:
-                if (board_bot.position in self.portal) :
-                    self.goal_position = self.redButton_position[0]
-                else :
-                    self.logicRedButtonPortal(board_bot, board)
-            
+            if final_distance_red <= final_distance_blue and board_bot.properties.diamonds != 4:
+                final_distance_diamond = final_distance_red
+                self.goal_position = goal_position_red
             else:
-                if final_distance_red <= final_distance_blue and board_bot.properties.diamonds != 4:
-                    self.goal_position = goal_position_red
-                else:
-                    self.goal_position = goal_position_blue
+                final_distance_diamond = final_distance_blue
+                self.goal_position = goal_position_blue
         
         elif goal_position_blue == None :
+            final_distance_diamond = final_distance_red
             self.goal_position = goal_position_red
         
         else :
+            final_distance_diamond = final_distance_blue
             self.goal_position = goal_position_blue
+        
+        if self.distance_to_base <= final_distance_diamond and board_bot.position != board_bot.properties.base and board_bot.properties.diamonds >= 3:
+            if (board_bot.position in self.portal) :
+                self.goal_position = board_bot.properties.base
+            else :
+                self.logicBasePortal(board_bot, board)
+        
+        elif self.distance_to_redButton <= final_distance_diamond:
+            if (board_bot.position in self.portal) :
+                self.goal_position = self.redButton_position[0]
+            else :
+                self.logicRedButtonPortal(board_bot, board)
     
 
-    def diamondInArea(self, position : Position, radius : int): # Mengecek apakah ada diamond di sekitar bot
+    # Mencari jumlah diamond di area tertentu
+    def diamondInArea(self, position : Position, radius : int):
         blue = [b for b in self.blue_diamonds if ((position.x - radius) <= b.x <= (position.x + radius)) and ((position.y - radius) <= b.y <= (position.y + radius)) ]
         red = [r for r in self.red_diamonds if (position.x - radius) <= r.x <= (position.x + radius) and (position.y - radius) <= r.y <= (position.y + radius) ]
         return len(blue) + len(red)
 
 
-    def next_move(self, board_bot: GameObject, board: Board): # Mencari langkah selanjutnya yang akan diambil oleh bot, mengimplementasikan fungsi-fungsi yang telah dibuat sebelumnya
+    # Menentukan langkah selanjutnya dari bot dengan mengimplementasikan fungsi-fungsi di atas
+    def next_move(self, board_bot: GameObject, board: Board):
         props = board_bot.properties
         base = board_bot.properties.base
         self.blue_diamonds = [diamond.position for diamond in board.game_objects if diamond.type == 'DiamondGameObject' if diamond.properties.points == 1]
         self.red_diamonds = [diamond.position for diamond in board.game_objects if diamond.type == 'DiamondGameObject' if diamond.properties.points == 2]
         self.portal = self.getPortal(board_bot,board)
         
-        # if redButton_position[0].x < redButton_position[0].y:
-        #     radius = redButton_position[0].y
-        # else :
-        #     radius = redButton_position[0].x
         self.distance_to_base = self.getDistance(board_bot.position, board_bot.properties.base)
         distance_to_base_portal = self.getDistance(board_bot.position, self.portal[0]) + self.getDistance(self.portal[1], base)
-        
         if distance_to_base_portal <= self.distance_to_base:
             self.distance_to_base = distance_to_base_portal
             self.isBasePortal = True
@@ -225,7 +231,6 @@ class OmegaLogic(BaseLogic):
             self.distance_to_redButton = distance_to_redButton_portal
             self.isRedButtonPortal = True
 
-        # print(board_bot.properties.milliseconds_left/1000, self.distance_to_base)
         if props.diamonds == 5 or (board_bot.properties.milliseconds_left/1000 - 1 <= self.distance_to_base) or (props.diamonds == 4 and len(self.blue_diamonds) == 0):
             if (board_bot.position in self.portal) :
                 self.goal_position = base
@@ -246,5 +251,5 @@ class OmegaLogic(BaseLogic):
             self.goal_position.x,
             self.goal_position.y,
         )
-        # print(board_bot.properties.milliseconds_left)
+        print(board_bot.properties.milliseconds_left)
         return delta_x, delta_y
